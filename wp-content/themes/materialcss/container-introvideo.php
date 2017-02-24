3f8b9448297953f45d4dea1bef27fba73befcd0f
abraham <abe@unscene.us>
2017-02-23 17:14:58 -0800

    no message

---
+++ wp-content/themes/materialcss/container-introvideo.php
@@ -0,0 +1,26 @@
+<div id="introvideo" class="black-text parallax-container">
+  <div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+            <div class="s12 col center">
+              <h1>Spiritual Advisor. Coach. Teacher.</h1>
+            </div>
+            <div class="col s12 m7">
+              
+            </div>
+            <div class="col s12 m5">
+                <p class="script">Like what you see</p>
+                <a href="/e-books/" class="btn waves-effect waves-light btn-floating"><i class="sc-twitter grey-text text-darken-4"></i></a>
+                <a href="/e-books/" class="btn waves-effect waves-light btn-floating"><i class="sc-facebook grey-text text-darken-4"></i></a>
+                <a href="/e-books/" class="btn waves-effect waves-light btn-floating"><i class="sc-linkedin grey-text text-darken-4"></i></a>
+            </div>
+      </div> <!-- end row post-loop -->
+
+    </div> <!-- end section -->
+  </div>
+
+ <!-- end container -->
+  <div class="parallax"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/vital-transformations-mt-fuji.jpg" alt="Vital Transformation"></div>
+</div>
